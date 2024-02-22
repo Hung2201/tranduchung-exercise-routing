20b86ec204b04ea4bed57aa939d8b3ad1514ea0e
Admin <admin@vinasupport.com>
2024-02-22 21:49:42 +0700

    update part 4 5 6

--- routes/api.php
+++ routes/api.php
@@ -17,30 +17,53 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/user', function(){
-    global $users;
-    return $users;
-});
-
-Route::get('/user/{userIndex}', function($userIndex){
-    global $users;
-    if (isset($users[$userIndex])){
-        return $users[$userIndex];
-    }
-    else {
-        return "Can not find user index " . $userIndex;
-    }
-})->whereNumber('userIndex');
-
-Route::get('/user/{userName}', function($userName){
-    global $users;
-    foreach ($users as $user){
-        if ($userName == $user['name']){
-            return $user;
+Route::prefix('/user') -> group(function(){
+    Route::get('/', function(){
+        global $users;
+        return $users;
+    });
+    Route::get('/{userIndex}', function($userIndex){
+        global $users;
+        if (isset($users[$userIndex])){
+            return $users[$userIndex];
+        }
+        else {
+            return "Can not find user index " . $userIndex;
+        }
+    })->whereNumber('userIndex');
+    Route::get('/{userName}', function($userName){
+        global $users;
+        foreach ($users as $user){
+            if ($userName == $user['name']){
+                return $user;
+            }
+            else{
+                return "cannot find this user ". $userName;
+            }
+        }
+    })->whereAlpha('userName');
+    Route::fallback(function(){
+        return "You cannot get user like this";
+    });
+    Route::get('/{userIndex}/post/{postIndex}', function($userIndex, $postIndex){
+        global $users;
+        if (isset($users[$userIndex])){
+            $user = $users[$userIndex];
+            if (isset($user['posts'][$postIndex])){
+                return $user['posts'][$postIndex];
+            }
+            else {
+                return "Cannot find post with index " . $postIndex . "for user with index " . $userIndex;
+            }
         }
         else{
-            return "cannot find this user ". $userName;
+            return "Cannot find user with index " . $userIndex;
         }
-    }
-   
-})->whereAlpha('userName');
+    })->whereNumber(['userIndex', 'postIndex']);
+});
+
+
+
+
+
+
